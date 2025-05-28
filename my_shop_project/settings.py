df76c85f29c6e4b7d9748e427887af649a0c9393
Georjay <geonoble988@gmail.com>
2025-05-28 16:38:41 +0100

    Adding an image field

--- my_shop_project/settings.py
+++ my_shop_project/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles' # Or os.path.join(BASE_DIR, 'mediafiles') if using older Django/Python
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2) # e.g., 99999999.99
-    image = models.CharField(max_length=255, blank=True, null=True) # For now, we'll store a path or URL to an image
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True) # Automatically set when product is created
     updated_at = models.DateTimeField(auto_now=True)     # Automatically set when product is updated
