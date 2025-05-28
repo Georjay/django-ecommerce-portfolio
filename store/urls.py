9d04bf9697fd96116f8f9c11e5590595f6e4db35
Georjay <geonoble988@gmail.com>
2025-05-28 16:26:05 +0100

    Upgrading by add a css file

--- store/urls.py
+++ store/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # When someone visits the 'root' of this app (e.g., /store/),
     # call the product_list view.
     path('', views.product_list, name='product_list'),
+    path('<int:product_id>/', views.product_detail, name='product_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def product_list(request):
@@ -13,3 +13,13 @@ def product_list(request):
     }
 
     return render(request, 'store/product_list.html', context)
+
+def product_detail(request, product_id):
+    # Get a single product by its id (primary key)
+    # If the product with that id doesn't exist, it will show a 404 page
+    product = get_object_or_404(Product, id=product_id)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'store/product_detail.html', context)
